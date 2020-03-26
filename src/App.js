891f1d3d7ef5145c5ee91454ab704c249ee05eea
Sailesh Dahal <saileshbro@gmail.com>
2020-03-26 14:59:42 +0545

    header component and links to all pages

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shoppage/shop.component";
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Header from "./components/header/header.component";
+
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+const config = {
+  apiKey: "AIzaSyD9qbSvU5WAQqrCThIUBvqjfz9iSPWTuUI",
+  authDomain: "crown-clothing-37cc3.firebaseapp.com",
+  databaseURL: "https://crown-clothing-37cc3.firebaseio.com",
+  projectId: "crown-clothing-37cc3",
+  storageBucket: "crown-clothing-37cc3.appspot.com",
+  messagingSenderId: "864971740517",
+  appId: "1:864971740517:web:4aa3473420241ad8a43d8c",
+  measurementId: "G-GH177KL4ER"
+};
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export default firebase;
