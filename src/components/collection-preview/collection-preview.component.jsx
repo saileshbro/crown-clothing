58ed250755553c50f7323088d04de785fb8eadda
Sailesh Dahal <saileshbro@gmail.com>
2020-04-04 15:28:54 +0545

    sorted data to its reducers. created selectors and updated components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import './collection-preview.styles.scss'
 import CollectionItem from '../collection-item/collection-item.component'
 const CollectionPreview = ({ title, items }) => (
-	<div className='collection-preview'>
-		<h1 className='title'>{title.toUpperCase()}</h1>
-		<div className='preview'>
-			{items
-				.filter((item, i) => i < 4)
-				.map((item) => (
-					<CollectionItem key={item.id} item={item} />
-				))}
-		</div>
-	</div>
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, i) => i < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </div>
+  </div>
 )
 export default CollectionPreview
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,17 @@
+import { createSelector } from 'reselect'
+
+const selectShop = state => state.shop
+
+export const selectCollections = createSelector(
+  [selectShop],
+  shop => shop.collections,
+)
+export const selectCollectionsForOverview = createSelector(
+  [selectCollections],
+  collections => Object.values(collections),
+)
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam],
+  )
